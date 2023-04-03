6f6d664f108a1a7febb674205a6c584b896268b3
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-03 14:28:22 +0300

    Image loader interface

---
+++ core/io/image_loader.h
@@ -0,0 +1,16 @@
+#ifndef IMAGE_LOADER_H
+#define IMAGE_LOADER_H
+
+#include "bitmap.h"
+#include "../string/string_types.h"
+
+class ImageLoader
+{
+public:
+	virtual ~ImageLoader() = default;
+
+	virtual GNF_Image* load(const String& path) = 0;
+private:
+};
+
+#endif // IMAGE_LOADER_H
