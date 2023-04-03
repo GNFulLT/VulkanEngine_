3709723b2baf92fb65fc89eced0a9ea439780e07
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-03 14:28:01 +0300

    Image representing

--- plugin/pinclude/basic_shared_types.h
+++ plugin/pinclude/basic_shared_types.h
@@ -1,12 +1,27 @@
 #ifndef BASIC_SHARED_TYPES_H
 #define BASIC_SHARED_TYPES_H
 
-struct GNF_UVec2
+extern "C"
 {
-	unsigned int x;
-	unsigned int y;
-		
-};
+	struct GNF_UVec2
+	{
+		unsigned int x;
+		unsigned int y;
+	};
 
+	enum IMAGE_FORMAT
+	{
+		IMAGE_FORMAT_UNSIGNED_BYTE,
+		IMAGE_FORMAT_FLOAT,
+	};
 
+	struct GNF_Image
+	{
+		size_t      width;
+		size_t      height;
+		IMAGE_FORMAT format;
+		uint8_t* pixels;
+	};
+
+}
 #endif
