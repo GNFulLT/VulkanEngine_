63f5ed09bbf034de25e7312897195c0a56c5f619
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-04 12:45:39 +0300

    Resource Management

---
+++ manager/resource_manager.cpp
@@ -0,0 +1,76 @@
+#include "resource_manager.h"
+
+#include "../core/string/string_utils.h"
+#include "../graphic/vulkan_image.h"
+#include "memory_manager.h"
+ResourceManager::ResourceManager()
+{
+	m_imageLoaderMap.emplace(hash_string("DEFAULT"),&m_baseImageLoader); 
+	m_defaultImageLoader = &m_baseImageLoader;
+
+}
+
+Resource* ResourceManager::create_image_resource(const String& resourceKey, const String& resourcePath, Object* who,const String& loaderName)
+{
+	//! CHECK IF GIVEN PATH RESOURCE IS LOADED BEFORE
+	
+	auto pathHash = hash_string(resourcePath);
+	auto keyHash = hash_string(resourceKey);
+	if (auto res = m_resourcePathMap.find(pathHash);res != m_resourcePathMap.end())
+	{
+		if (m_resourceKeysMap.find(keyHash) != m_resourceKeysMap.end())
+		{
+			return nullptr;
+		}
+		//m_resourceKeysMap.emplace(keyHash, res->second);
+		return res->second;
+	}
+	
+	//! Select Image Loader If any custom selected
+	ImageLoader* imageLoader = m_defaultImageLoader;
+
+	if (loaderName != "")
+	{
+		if (auto loader = m_imageLoaderMap.find(hash_string(loaderName)); loader != m_imageLoaderMap.end())
+		{
+			imageLoader = loader->second;
+		}
+	}
+
+	auto img = imageLoader->load(resourcePath);
+	
+	VulkanImage* vimg = MemoryManager::get_singleton()->new_object<VulkanImage>(resourceKey,img);
+	//X TODO : LOG
+	if (vimg->is_failed())
+	{
+		MemoryManager::get_singleton()->destroy_object(vimg);
+		imageLoader->unload(img);
+		return nullptr;
+	}
+
+	imageLoader->unload(img);
+
+	vimg->register_owner(who);
+	vimg->set_name(resourceKey.c_str());
+	vimg->m_path = resourcePath;
+
+	m_resourceKeysMap.emplace(keyHash,vimg);
+	m_resourcePathMap.emplace(pathHash,vimg);
+
+	return vimg;
+}
+
+
+void ResourceManager::destroy_resource(Resource* res)
+{
+	auto pathHash = hash_string(String(res->get_path()));
+	auto keyHash = hash_string(String(res->get_object_name()));
+	assert(m_resourceKeysMap.find(keyHash) != m_resourceKeysMap.end());
+	assert(m_resourcePathMap.find(pathHash) != m_resourcePathMap.end());
+
+	m_resourceKeysMap.erase(keyHash);
+	m_resourcePathMap.erase(pathHash);
+
+
+	MemoryManager::get_singleton()->destroy_object(res);
+}
---
+++ manager/resource_manager.h
@@ -0,0 +1,32 @@
+#ifndef RESOURCE_MANAGER_H
+#define RESOURCE_MANAGER_H
+
+#include "system_manager.h"
+
+#include "../core/typedefs.h"
+#include "../core/io/resource.h"
+#include "../core/io/base_image_loader.h"
+#include "../core/string/string_types.h"
+#include <unordered_map>
+
+class ResourceManager : public SystemManager
+{
+	SINGLETON(ResourceManager)
+	OBJECT_DEF(ResourceManager,SystemManager)
+public:
+	ResourceManager();
+
+
+	Resource* create_image_resource(const String& resourceKey,const String& resourcePath, Object* who = nullptr ,const String& loaderName = "");
+	
+	void destroy_resource(Resource* res);
+private:
+	std::unordered_map<size_t, Resource*> m_resourceKeysMap;
+	std::unordered_map<size_t, Resource*> m_resourcePathMap;
+	std::unordered_map<size_t, ImageLoader*> m_imageLoaderMap;
+
+	ImageLoader* m_defaultImageLoader;
+
+	BaseImageLoader m_baseImageLoader;
+};
+#endif // RESOURCE_MANAGER_H
