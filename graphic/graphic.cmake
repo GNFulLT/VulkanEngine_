5d11a9b7277f0496128bdda34292d64ad5c51250
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-06 15:06:48 +0300

    editor_image

--- graphic/graphic.cmake
+++ graphic/graphic.cmake
@@ -2,5 +2,7 @@ file(GLOB VulkanEngine_GRAPHIC_SRC
 	"graphic/vulkan_renderpass.h"
 	"graphic/vulkan_image.h"
 	"graphic/vulkan_image.cpp"
+	"graphic/editor_image.h"
+	"graphic/editor_image.cpp"
 
 )
