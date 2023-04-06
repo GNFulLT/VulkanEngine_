3d7de0f07681f6ad9fb52ac29f14fcebed8c1670
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-06 15:02:25 +0300

    stb loading

--- core/io/base_image_loader.cpp
+++ core/io/base_image_loader.cpp
@@ -4,15 +4,16 @@
 GNF_Image* BaseImageLoader::load(const String& path)
 {
 	int texWidth, texHeight, texChannels;
-	stbi_uc* pixels = stbi_load(path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+	auto* pixels = stbi_load(path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
 	GNF_Image* img = MemoryManager::get_singleton()->fnew_c<GNF_Image>();
 	img->width = texWidth;
 	img->height = texHeight;
 	img->format = IMAGE_FORMAT_R8G8B8A8_UNSIGNED_BYTE;
-	img->pixels = pixels;
+	img->pixels = (uint8_t*)pixels;
 	return img;
 }
 void BaseImageLoader::unload(GNF_Image* img)
 {
+	stbi_image_free(img->pixels);
 	MemoryManager::get_singleton()->fdel_c(img);
 }
