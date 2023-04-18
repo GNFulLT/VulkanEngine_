14d6f6e00f41fa08098c6b0b89d2a06210bf9713
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-18 22:04:08 +0300

    Base Client Editor

---
+++ gui/editor.h
@@ -0,0 +1,31 @@
+#ifndef NO_USE_EDITOR
+
+#ifndef EDITOR_H
+#define EDITOR_H
+
+
+#include "../main/application.h"
+
+
+class Editor : public Application
+{
+public:
+	virtual int before_start_impl() override;
+	virtual void update_impl() override;
+	virtual void render_impl() override;
+	virtual bool begin_frame_impl() override;
+
+	virtual void fixed_update_impl() override;
+
+	virtual void before_exit_impl() override;
+
+	
+private:
+	void set_next_image_e();
+	void present_image_e();
+	void process_events_e();
+};
+
+
+#endif // EDITOR_H
+#endif // NO_USE_EDITOR
