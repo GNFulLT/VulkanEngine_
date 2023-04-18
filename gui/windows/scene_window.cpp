56cebdd7acdc6aaff005bb4ed944fe26eed2ce5f
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-18 22:06:44 +0300

    Container for getting gpu to cpu image

--- gui/windows/scene_window.cpp
+++ gui/windows/scene_window.cpp
@@ -10,6 +10,11 @@ SceneWindow::SceneWindow(String name, RenderScene* pRenderScene) : Window(name),
 {
 }
 
+void SceneWindow::delete_buff()
+{
+	p_render_scene->delete_buff();
+}
+
 void SceneWindow::render()
 {
 	ImGui::Image(p_render_scene->get_image(),ImVec2(m_size.get()->x, m_size.get()->y));
@@ -30,6 +35,11 @@ void SceneWindow::pre_render()
 	auto size = ImGui::GetContentRegionAvail();
 	m_size.set(GNF_UVec2{ .x = (unsigned int)size.x, .y = (unsigned int)size.y });
 	p_render_scene->on_resize(*m_size.get());
+}
+
+uint8_t* SceneWindow::get_image_bytes()
+{
+	return p_render_scene->get_image_bytes();
 }
 
 void SceneWindow::on_size_changed()
--- gui/windows/scene_window.h
+++ gui/windows/scene_window.h
@@ -15,11 +15,12 @@ public:
 	{
 		return p_render_scene;
 	}
+	void delete_buff();
 	void render() override final;
 	bool on_created() override final;
 	void render_ex(VkQueue queue, VkCommandBuffer buff, VkFence fence);
 	void pre_render() override final;
-
+	uint8_t* get_image_bytes();
 	void on_size_changed() override final;
 private:
 private:
