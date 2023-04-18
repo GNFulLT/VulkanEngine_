363d91854a6e6b61a638ca468f2f860219ad6d9a
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-18 22:02:10 +0300

    Support multiple framebuffer

--- graphic/vulkan_renderpass.h
+++ graphic/vulkan_renderpass.h
@@ -19,6 +19,100 @@ struct VulkanRenderpass
 		_isFailed = true;
 		_subpassContents = VkSubpassContents::VK_SUBPASS_CONTENTS_INLINE;
 	}
+
+	void create(VkDevice dev, const std::vector<VkImageView>& views, std::vector<GNF_UVec2>& sizes, const std::vector<VkClearValue>& clearValues,
+		VkFormat format, VkImageLayout finalLayout, VkImageLayout attachmentReferenceLayout, VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE)
+	{
+		_info = {};
+
+		_subpassContents = subpassContents;
+
+		_clearValues = clearValues;
+		VkAttachmentDescription color_attachment = {};
+		//the attachment will have the format needed by the swapchain
+		color_attachment.format = format;
+		//1 sample, we won't be doing MSAA
+		color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+		color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+		color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+
+		//X TODO:  No stencil for now
+		color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+		color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+		// Used as frame buffer
+		color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+
+		color_attachment.finalLayout = finalLayout;
+
+		VkRenderPassCreateInfo create_inf = {};
+		create_inf.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+
+
+		VkAttachmentReference color_attachment_ref = {};
+		//attachment number will index into the pAttachments array in the parent renderpass itself
+		color_attachment_ref.attachment = 0;
+		color_attachment_ref.layout = attachmentReferenceLayout;
+
+		VkSubpassDescription subpass = {};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+		subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &color_attachment_ref;
+
+		//connect the color attachment to the info
+		create_inf.attachmentCount = 1;
+		create_inf.pAttachments = &color_attachment;
+		//connect the subpass to the info
+		create_inf.subpassCount = 1;
+		create_inf.pSubpasses = &subpass;
+		VkRenderPass handle;
+
+		if (VK_SUCCESS != vkCreateRenderPass(dev, &create_inf, nullptr, &handle))
+		{
+			_isFailed = true;
+		}
+		else
+		{
+			_isFailed = false;
+		}
+
+		if (!_isFailed)
+		{
+			_info.resize(views.size());
+			for (int i = 0; i < views.size(); i++)
+			{
+				VkFramebufferCreateInfo fb_info = {};
+				fb_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+				fb_info.pNext = nullptr;
+
+				fb_info.renderPass = handle;
+				fb_info.attachmentCount = 1;
+				fb_info.width = sizes[i].x;
+				fb_info.height = sizes[i].y;
+				fb_info.layers = 1;
+				fb_info.pAttachments = &views[i];
+				VkFramebuffer frameBuffer;
+				if (vkCreateFramebuffer(dev, &fb_info, nullptr, &frameBuffer) != VK_SUCCESS)
+				{
+					_isFailed = true;
+					break;
+				}
+				else
+				{
+					_isFailed = false;
+					_info[i].sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+					_info[i].renderPass = handle;
+					_info[i].framebuffer = frameBuffer;
+					_info[i].renderArea.extent.width = sizes[i].x;
+					_info[i].renderArea.extent.height = sizes[i].y;
+					_info[i].clearValueCount = (uint32_t)_clearValues.size();
+					_info[i].pClearValues = _clearValues.data();
+				}
+			}
+		}
+	}
+
 	void create(VkDevice dev,VkImageView imageView,uint32_t width,uint32_t height, const std::vector<VkClearValue>& clearValues,
 		VkFormat format,VkImageLayout finalLayout,VkImageLayout attachmentReferenceLayout,VkSubpassContents subpassContents = VK_SUBPASS_CONTENTS_INLINE)
 	{
@@ -77,6 +171,8 @@ struct VulkanRenderpass
 		}
 		if (!_isFailed)
 		{
+			_info.resize(1);
+
 			VkFramebufferCreateInfo fb_info = {};
 			fb_info.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
 			fb_info.pNext = nullptr;
@@ -95,13 +191,13 @@ struct VulkanRenderpass
 			else
 			{
 				_isFailed = false;
-				_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-				_info.renderPass = handle;
-				_info.framebuffer = frameBuffer;
-				_info.renderArea.extent.width = width;
-				_info.renderArea.extent.height = height;
-				_info.clearValueCount = (uint32_t)_clearValues.size();
-				_info.pClearValues = _clearValues.data();
+				_info[0].sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+				_info[0].renderPass = handle;
+				_info[0].framebuffer = frameBuffer;
+				_info[0].renderArea.extent.width = width;
+				_info[0].renderArea.extent.height = height;
+				_info[0].clearValueCount = (uint32_t)_clearValues.size();
+				_info[0].pClearValues = _clearValues.data();
 			}
 		}
 		
@@ -112,16 +208,20 @@ struct VulkanRenderpass
 
 	_F_INLINE_ void destroy(VkDevice dev) _NO_EXCEPT_
 	{
-		if (_info.renderPass != nullptr)
-			vkDestroyRenderPass(dev, _info.renderPass, nullptr);
-		if(_info.framebuffer != nullptr)
-			vkDestroyFramebuffer(dev, _info.framebuffer, nullptr);
+		if (_info[0].renderPass != nullptr)
+			vkDestroyRenderPass(dev, _info[0].renderPass, nullptr);
+		for (int i = 0; i < _info.size(); i++)
+		{
+			if (_info[i].framebuffer != nullptr)
+				vkDestroyFramebuffer(dev, _info[i].framebuffer, nullptr);
+		}
+		
 		_isFailed = true;
 	}
 
-	_F_INLINE_ void begin(VkCommandBuffer cmd) _NO_EXCEPT_
+	_F_INLINE_ void begin(VkCommandBuffer cmd,int index = 0) _NO_EXCEPT_
 	{
-		vkCmdBeginRenderPass(cmd, &_info, _subpassContents);
+		vkCmdBeginRenderPass(cmd, &_info[index], _subpassContents);
 	}
 
 	_F_INLINE_ void end(VkCommandBuffer cmd) _NO_EXCEPT_
@@ -129,10 +229,10 @@ struct VulkanRenderpass
 		vkCmdEndRenderPass(cmd);
 	}
 
-	_F_INLINE_ void set_size(const GNF_UVec2* size) noexcept
+	_F_INLINE_ void set_size(const GNF_UVec2* size,int index = 0) _NO_EXCEPT_
 	{
-		_info.renderArea.extent.width = (uint32_t)size->x;
-		_info.renderArea.extent.height = (uint32_t)size->y;
+		_info[index].renderArea.extent.width = (uint32_t)size->x;
+		_info[index].renderArea.extent.height = (uint32_t)size->y;
 	}
 
 	_F_INLINE_ bool is_failed() const noexcept
@@ -140,14 +240,17 @@ struct VulkanRenderpass
 		return _isFailed;
 	}
 	
-
-
+	_F_INLINE_ VkRenderPass get_handle() const _NO_EXCEPT_
+	{
+		return _info[0].renderPass;
+	}
 private:
-	VkRenderPassBeginInfo _info;
+	std::vector<VkRenderPassBeginInfo> _info;
 	VkSubpassContents _subpassContents;
 	std::vector<VkClearValue> _clearValues;
 	std::vector<VkPipelineStageFlags> _stageFlags;
 	bool _isFailed = false;
+	
 };
 
 #endif // VULKAN_RENDERPASS_H
