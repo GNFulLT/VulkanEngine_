430091a0ec05976b14686b7b696d91940ac37d1c
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-20 08:34:05 +0300

    Editor

--- gui/editor.h
+++ gui/editor.h
@@ -10,20 +10,25 @@
 class Editor : public Application
 {
 public:
+	Editor();
+	virtual ~Editor() = default;
 	virtual int before_start_impl() override;
 	virtual void update_impl() override;
 	virtual void render_impl() override;
 	virtual bool begin_frame_impl() override;
 
 	virtual void fixed_update_impl() override;
-
 	virtual void before_exit_impl() override;
-
+protected:
+	virtual void render_scene_impl() override; 
+	virtual void override_loaders() override;
 	
 private:
 	void set_next_image_e();
 	void present_image_e();
 	void process_events_e();
+
+	GNF_UVec2 m_offscreenSize;
 };
 
 
