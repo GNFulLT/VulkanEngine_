105db54bbfae81dc4d8b3d4bca9b26c2bc3ba3fa
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2023-04-25 07:51:01 +0300

    semaphores and factories

--- main/application.h
+++ main/application.h
@@ -16,15 +16,20 @@
 #include <functional>
 #include <atomic>
 #include <stack>
+#include <utility>
+#include <optional>
 
-BETTER_ENUM(APPLICATION_MANAGER,uint8_t, 
-	APPLICATION_MANAGER_LOGGER_MANAGER, 
-	APPLICATION_MANAGER_WINDOW_MANAGER, 
-	APPLICATION_MANAGER_RENDER_DEVICE, 
+BETTER_ENUM(APPLICATION_MANAGER, uint8_t,
+	APPLICATION_MANAGER_LOGGER_MANAGER,
+	APPLICATION_MANAGER_WINDOW_MANAGER,
+	APPLICATION_MANAGER_RENDER_DEVICE,
+	APPLICATION_MANAGER_PRESENTATION_MANAGER,
 	APPLICATION_MANAGER_RESOURCE_MANAGER,
 	APPLICATION_MANAGER_COUNT)
 
-	
+using ManagerFactoryCreatorFnc = std::function<SystemManager*()>;
+using ManagerFactoryDestroyerFnc = std::function<void(SystemManager*)>;
+
 
 class Application
 {
@@ -39,7 +44,8 @@ public:
 	int run();
 	int before_start();
 
-
+	static void add_wait_semaphore(VkSemaphore semaphore);
+	static void add_signal_semaphore(VkSemaphore semaphore);
 	//X Update
 	void update();
 
@@ -52,6 +58,8 @@ public:
 
 	void before_exit();
 protected:
+	void set_factory(APPLICATION_MANAGER::_enumerated manager, const std::pair<ManagerFactoryCreatorFnc, ManagerFactoryDestroyerFnc>& factory);
+
 	virtual int before_start_impl() { return 0; }
 	virtual void update_impl() {}
 	virtual void render_impl() {}
@@ -71,7 +79,6 @@ protected:
 	std::function<void()> process_events;
 	std::function<void()> present_image;
 	std::function<void()> set_next_image;
-	std::function<bool()> load_window;
 	std::function<bool()> need_handle_for_resize;
 	std::function<void()> handle_resize;
 	std::function<bool()> needed_render;
@@ -82,11 +89,13 @@ private:
 	tf::Future<void> initialize_managers(tf::Taskflow& flow);
 private:
 	std::array<std::atomic_bool, APPLICATION_MANAGER::APPLICATION_MANAGER_COUNT> m_managerLoads;
+	std::array<std::optional<std::pair<ManagerFactoryCreatorFnc, ManagerFactoryDestroyerFnc>>, APPLICATION_MANAGER::APPLICATION_MANAGER_COUNT> m_managerFactories;
 	boost::timer::cpu_times m_applicationStartupTime;
 	std::stack<SystemManager*> m_loadedSystemManagers;
 	inline static volatile bool m_wantsExit = false;
+	inline static std::vector<VkSemaphore> m_waitSemaphores;
+	inline static std::vector<VkSemaphore> m_signalSemaphores;
 
-	
 };
 
 
